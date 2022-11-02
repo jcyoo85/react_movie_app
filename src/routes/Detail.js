5073fe130eb15bfbf85ec414c0c14ca82e81b90a
wheely <wheely@gccompany.co.kr>
2022-11-02 17:57:28 +0900

    useEffect []empty array warn fixed

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,25 +1,25 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
-  const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState({});
 
-  const getMovie = async () => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
-    )
-    const json = await response.json();
-    setMovie(json.data.movie);
-    setLoading(false);
-  }
-
-  console.log(movie);
+  const getMovie = useCallback( 
+    async () => {
+      const response = await fetch(
+        `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+      )
+      const json = await response.json();
+      setMovie(json.data.movie);
+      setLoading(false);
+    }
+  ,[id])
 
   useEffect(() => {
     getMovie();
-  });
+  }, [getMovie]);
 
   return (
     <div>
